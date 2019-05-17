e27c37ef4f468fe40ddc923636eff9695539d62d
Daniel Chote <daniel@chote.com>
2019-05-16 20:25:02 -0400

    fix indentation

--- rippr.sh
+++ rippr.sh
@@ -10,29 +10,29 @@ sleep 1
 
 if [ -f "/mnt/sr0/BDMV/META/DL/bdmt_eng.xml" ]; then
 
-	title="$(xpath -q -e '/disclib/di:discinfo/di:title/di:name/text()' /mnt/sr0/BDMV/META/DL/bdmt_eng.xml)"
-	title=${title/'™'/''}
-	title=${title/' - Blu-ray'/''}
-	title="${title// /_}"
-	
-	tar czf "/tmp/$title.tar.gz" -C /mnt/sr0/BDMV/META/DL .
+  title="$(xpath -q -e '/disclib/di:discinfo/di:title/di:name/text()' /mnt/sr0/BDMV/META/DL/bdmt_eng.xml)"
+  title=${title/'™'/''}
+  title=${title/' - Blu-ray'/''}
+  title="${title// /_}"
 
-	echo "Disc title is: $title"
+  tar czf "/tmp/$title.tar.gz" -C /mnt/sr0/BDMV/META/DL .
 
-	umount /dev/sr0
+  echo "Disc title is: $title"
 
-	if [ -n "$title" ]; then
-	    rip_path="$rip_target/$title"
-	    mkdir -p $rip_path
-	    echo "Path is: $rip_path"
-			
-			if [ -f "/tmp/$title.tar.gz" ]; then
-				cp --no-preserve=mode,ownership "/tmp/$title.tar.gz" "$rip_path/meta.tar.gz"
-				rm "/tmp/$title.tar.gz"
-			fi
-			
-	    makemkvcon --minlength=3600 -r --decrypt --directio=true mkv disc:0 all $rip_path
-	fi
+  umount /dev/sr0
+
+  if [ -n "$title" ]; then
+    rip_path="$rip_target/$title"
+    mkdir -p $rip_path
+    echo "Path is: $rip_path"
+
+    if [ -f "/tmp/$title.tar.gz" ]; then
+      cp --no-preserve=mode,ownership "/tmp/$title.tar.gz" "$rip_path/meta.tar.gz"
+      rm "/tmp/$title.tar.gz"
+    fi
+
+    makemkvcon --minlength=3600 -r --decrypt --directio=true mkv disc:0 all $rip_path
+  fi
 fi
 
 #eject
