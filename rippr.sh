35aa2240164c980c27bed8a3fc903e74680335bd
Daniel Chote <daniel@chote.com>
2019-06-15 10:42:36 -0400

    add disc title

--- rippr.sh
+++ rippr.sh
@@ -9,7 +9,6 @@ mount /dev/sr0 /mnt/sr0
 sleep 1
 
 if [ -f "/mnt/sr0/BDMV/META/DL/bdmt_eng.xml" ]; then
-
   title="$(xpath -q -e '/disclib/di:discinfo/di:title/di:name/text()' /mnt/sr0/BDMV/META/DL/bdmt_eng.xml)"
   title=${title/'™'/''}
   title=${title/' - Blu-ray'/''}
@@ -19,22 +18,27 @@ if [ -f "/mnt/sr0/BDMV/META/DL/bdmt_eng.xml" ]; then
 
   tar czf "/tmp/$title.tar.gz" -C /mnt/sr0/BDMV/META/DL .
 
-  echo "Disc title is: $title"
-
-  umount /dev/sr0
-
-  if [ -n "$title" ]; then
-    rip_path="$rip_target/$title"
-    mkdir -p $rip_path
-    echo "Path is: $rip_path"
-
-    if [ -f "/tmp/$title.tar.gz" ]; then
-      cp --no-preserve=mode,ownership "/tmp/$title.tar.gz" "$rip_path/meta.tar.gz"
-      rm "/tmp/$title.tar.gz"
-    fi
-
-    makemkvcon --minlength=3600 -r --decrypt --directio=true mkv disc:0 all $rip_path
-  fi
+  echo "Disc title via META is: $title"
+else
+  title="$(blkid -o value -s LABEL /dev/sr0)"
 fi
 
-#eject
+umount /dev/sr0
+
+if [ -n "$title" ]; then
+  rip_path="$rip_target/$title"
+  mkdir -p $rip_path
+  echo "Output path is: $rip_path"
+
+  if [ -f "/tmp/$title.tar.gz" ]; then
+    cp --no-preserve=mode,ownership "/tmp/$title.tar.gz" "$rip_path/meta.tar.gz"
+    rm "/tmp/$title.tar.gz"
+  fi
+
+  makemkvcon --minlength=3600 -r --decrypt --directio=true mkv disc:0 all $rip_path
+else
+  echo "\n** No title **\n\n"
+fi
+
+
+eject
